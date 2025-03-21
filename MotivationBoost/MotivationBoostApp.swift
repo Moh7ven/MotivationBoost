168e246162a422127ae2a856818598dc7ec100d4
Moh7ven <Mohamedsangare0107@gmail.com>
2025-03-21 00:02:23 +0000

    Initial Commit

---
+++ MotivationBoost/MotivationBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationBoostApp.swift
+//  MotivationBoost
+//
+//  Created by MACBOOK PRO on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MotivationBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
